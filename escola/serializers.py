329b5c5c4d90ebe8aa854fc3c7f1c37f5abfa995
Leon Daros <leondaros@gmail.com>
2024-10-24 17:40:23 +0100

    Improve validations

--- escola/serializers.py
+++ escola/serializers.py
@@ -2,12 +2,21 @@ from rest_framework import serializers
 
 from escola.admin import Cursos
 from escola.models import Estudante, Curso, Matricula
-
+from escola.validators import cpf_invalido, nome_invalido, celular_invalido
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
         fields = ['id','nome','email','cpf','data_nascimento','celular']
+
+    def validate(self,dados):
+        if cpf_invalido(dados['cpf']):
+            raise serializers.ValidationError({'cpf':'O CPF deve ter 11 digitos!'})
+        if nome_invalido(dados['nome']):
+            raise serializers.ValidationError({'nome':'O nome só pode ter letras'})
+        if celular_invalido(dados['celular']):
+            raise serializers.ValidationError({'celular':'O celular precisa ter 13 digitos!'})
+        return dados
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
