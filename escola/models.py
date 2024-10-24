8dc684e41dbf07bae9bb1e362df9afba5f4f713b
Leon Daros <leondaros@gmail.com>
2024-10-24 12:38:00 +0100

    Create Matriatricula

--- escola/models.py
+++ escola/models.py
@@ -7,6 +7,8 @@ class Estudante(models.Model):
     data_nascimento=models.DateField()
     celular=models.CharField(max_length=14)
 
+    objects = models.Manager()
+
     def __str__(self):
         return self.nome
 
@@ -18,7 +20,19 @@ class Curso(models.Model):
     )
     codigo=models.CharField(max_length=10)
     descricao=models.CharField(max_length=100, blank=False)
-    nivel=models.CharField(max_length=1, blank=False, null=False, default='B')
+    nivel=models.CharField(max_length=1, choices=NIVEL,blank=False, null=False, default='B')
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.codigo
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    estudante = models.ForeignKey(Estudante, on_delete= models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete= models.CASCADE)
+    periodo=models.CharField(max_length=1, choices=PERIODO,blank=False, null=False, default='M')
