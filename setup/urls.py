e01429dc6604ac45ee2b4c1dd9f8efd07066884e
Leon Daros <leondaros@gmail.com>
2024-10-24 13:38:53 +0100

    Create relationship endpoint

--- setup/urls.py
+++ setup/urls.py
@@ -3,7 +3,7 @@ from webbrowser import register
 
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework import  routers
 
 router = routers.DefaultRouter()
@@ -11,7 +11,11 @@ router.register('estudantes', EstudanteViewSet, basename='Estudantes')
 router.register('cursos', CursoViewSet, basename='Cursos')
 router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('estudantes/<int:pk>/matriculas', ListaMatriculaEstudante.as_view()),
+    path('cursos/<int:pk>/matriculas', ListaMatriculaCurso.as_view())
+
 ]
